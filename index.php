54fae85254ee00b69a1176b2b0ad63c0f12654ae
rjrsand <99208059+rjrsand@users.noreply.github.com>
2023-08-24 18:33:03 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html><head>
+<base href="/">
+<script src="dmxAppConnect/dmxAppConnect.js"></script>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+<!-- New Wappler Page -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" href="bootstrap/5/css/bootstrap.min.css" />
+</head>
+<body is="dmx-app" id="index">
+
+<script src="bootstrap/5/js/bootstrap.min.js"></script>
+</body></html>
