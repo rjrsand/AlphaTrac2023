29a2a3b24e40a8eb62f5cb2cafbf335da61e3a3c
rjrsand <99208059+rjrsand@users.noreply.github.com>
2023-08-25 19:33:16 -0400

    made php.include

--- footer.php
+++ footer.php
@@ -1,10 +1,17 @@
 <!doctype html>
 <html>
 <head>
+<base href="/">
+<script src="dmxAppConnect/dmxAppConnect.js"></script>
 <meta charset="UTF-8">
 <title>Untitled Document</title>
-<!-- New Wappler Page -->
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="bootstrap/5/css/bootstrap.min.css" />
+<link rel="stylesheet" href="css/style.css" />
 </head>
-<body>
+<body is="dmx-app" id="footer">
+<script src="bootstrap/5/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
