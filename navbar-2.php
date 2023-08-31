73444b94acc86cb2cf3d292cc4c019bb4cbbbbbf
rjrsand <99208059+rjrsand@users.noreply.github.com>
2023-08-31 17:49:14 -0400

    created navbar-2

---
+++ navbar-2.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html>
+
+<head>
+    <base href="/">
+    <script src="dmxAppConnect/dmxAppConnect.js"></script>
+    <meta charset="UTF-8">
+    <title>Untitled Document</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="bootstrap/5/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <style>
+        .container {
+            display: flex;
+            flex-direction: row;
+            height: 11vh;
+        }
+
+        .left {
+            flex: 1;
+        }
+
+        .right {
+            flex: 1;
+        }
+    </style>
+    <script src="dmxAppConnect/dmxBootstrap5Navigation/dmxBootstrap5Navigation.js" defer></script>
+</head>
+
+<body is="dmx-app" id="navbar2">
+    <div class="container">
+        <div class="left">
+            <main class="mb-3 mt-3">
+                <div class="container">
+                    <div class="row">
+                        <div class="col"><img src="assets/alphatrac-logo.svg" width="220" height="24" class="img-fluid" id="logo"></div>
+                    </div>
+                </div>
+            </main>
+        </div>
+        <div class="right">
+            <div class="container justify-content-end bg-primary">
+                <div class="row">
+
+                    <div class="col">
+                        <div class="d-flex">
+                            <div class="nav bg-primary">
+                                <a class="nav-item nav-link fw-light" href="products.php">Products
+                                    <a class="nav-item nav-link fw-light" href="about.php">About</a>
+                                    <a class="nav-item nav-link fw-light" href="newsletter.php">Newsletter</a>
+                                    <a class="nav-item nav-link fw-light" href="contact.php">Contact</a>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <script src="bootstrap/5/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
