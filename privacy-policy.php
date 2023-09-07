11c901ca3a85ac3699fa79982537f79ca18ae146
rjrsand <99208059+rjrsand@users.noreply.github.com>
2023-09-07 12:14:36 -0400

    created privpol.php

--- privacy-policy.php
+++ privacy-policy.php
@@ -1,10 +1,107 @@
 <!doctype html>
 <html>
+
 <head>
-<meta charset="UTF-8">
-<title>Untitled Document</title>
-<!-- New Wappler Page -->
+    <base href="/">
+    <script src="dmxAppConnect/dmxAppConnect.js"></script>
+    <meta charset="UTF-8">
+    <title>Untitled Document</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="bootstrap/5/css/bootstrap.min.css" />
+    <script src="dmxAppConnect/dmxBootstrap5Navigation/dmxBootstrap5Navigation.js" defer></script>
+    <script src="dmxAppConnect/dmxBrowser/dmxBrowser.js" defer></script>
 </head>
-<body>
+
+<body is="dmx-app" id="index">
+    <div is="dmx-browser" id="browser1"></div>
+    <header class="bg-light">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <nav class="navbar navbar-expand-lg fixed-top bg-transparent ps-4 pe-4 navbar-light">
+                        <a class="navbar-brand ms-auto" href="#">
+                            <img width="220px" height="50px" src="assets/alphatrac-logo.svg"></a>
+                        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbar1_collapse" aria-controls="navbar1_collapse" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse justify-content-end fw-light" id="navbar1_collapse">
+                            <div class="navbar-nav">
+                                <a class="nav-item nav-link text-light" href="#">Products</a>
+                                <a class="nav-item nav-link text-light" href="#">About</a>
+                                <a class="nav-item nav-link text-light" href="#">Newsletter</a>
+                                <a class="nav-item nav-link text-light" href="#">Contact</a>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main class="vh-100">
+        <div class="h-100 container-fluid">
+            <div class="row h-100">
+                <div class="col-lg-6 main-col-pt align-self-center gx-5">
+                    <div class="d-flex">
+                        <h2 class="ms-2 style4">Privacy Policy</h2>
+                    </div>
+                    <div class="row border rounded g-0 mt-2 ms-2 me-2 pt-1 pb-1 border-2" id="article">
+                        <div class="col">
+                            <div class="d-flex">
+                                <article class="article">
+                                    <p class="fw-light ps-3 pe-2">
+                                        <b>Introduction</b><br><br>
+                                        Welcome to Carrier Beacon, a product of AlphaTrac. We are a web-based platform designed to assist with finding, organizing, monitoring, and contacting registered freight carriers across the United States and parts of Canada. Our services are designed to streamline your freight management process and provide you with comprehensive tools and solutions for your motor carrier needs.
+                                        This Terms of Service Agreement ("Agreement") is a legal agreement between you (either an individual or entity) and AlphaTrac, the company that owns and operates the Carrier Beacon platform. This Agreement sets forth the terms and conditions that govern your use of our services.
+                                        <br><br><b>Acceptance of Terms</b><br><br>
+                                        By accessing our website, using our services, or clicking to accept or agree to this Agreement when this option is made available to you, you acknowledge that you have read, understood, and agree to be bound by this Agreement and our Privacy Policy, which is incorporated into this Agreement by reference. If you do not agree with these terms, you must not access or use our services.
+                                        If you are using our services on behalf of a company, organization, or other legal entity, you represent and warrant that you are authorized to bind that entity to this Agreement, in which case
+                                        Welcome to AlphaTrac.
+                                        Software solutions for businesses and individuals looking to supercharge their motor carrier and logistics management.
+                                        Thanks for your interest.
+                                    </p>
+                                </article>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 text-end">
+                            <button id="btn1" class="btn border rounded text-secondary mt-4 mb-3 me-2" dmx-on:click="">Done<i class="fas fa-arrow-right ps-2"></i></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 bg-primary main-col-pt text-light text-center fw-light align-self-center h-100 gx-5">
+                    <div class="d-flex justify-content-center mt-5 mb-5">
+                        <a><img class="img-fluid" src="assets/alpha-logo.svg" width="253" height="253" id="article"></a>
+                    </div>
+                    <div class="d-flex justify-content-center mt-5 mb-5">
+                        <h3>Welcome to AlphaTrac.</h3>
+                    </div>
+                    <div class="d-flex  style5">
+                        <p>Software solutions for businesses and individuals looking to supercharge their motor carrier and logistics management.</p>
+                    </div>
+                    <div class="d-flex justify-content-center style5 fst-italic mt-5 mb-5">
+                        <p>Thanks for your interest.</p>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <a class="nav-item nav-link text-light" href="privacy-policy.php">Privacy Policy</a>
+                        <a class="nav-item nav-link text-light">|</a>
+                        <a class="nav-item nav-link text-light" href="terms-of-service.php">Terms of Service</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+
+    <script src="bootstrap/5/js/bootstrap.min.js"></script>
+
+
+    <script src="js/script.js"></script>
 </body>
+
 </html>
